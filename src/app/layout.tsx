c19babb5adc5e36dd4876b5d9d422d30f29ba543
mehulrana017 <mehulrana159@gmail.com>
2026-07-13 23:23:55 +0530

    fix: fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
     >
       <head>
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
